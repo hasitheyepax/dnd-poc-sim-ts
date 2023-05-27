afc25d1da47f04f04ae9e2c899ac29f74163e6de
KasunDissanayake <kasun.d@eyepax.com>
2023-05-27 23:04:25 +0530

    working example for dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css';
 import DraggableComponentList from './components/DraggableComponentList';
+import DragDropComponent from './components/DragDropComponent';
 import { data } from './util/data';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 function App() {
-  return (
+  // return (
+  //   <div className="App">
+  //     <DragDropContext onDragEnd={() => null}>
+  //       <DraggableComponentList listName="main" data={data} dataPath={''} />
+  //     </DragDropContext>
+  //   </div>
+  // );
+  return(
     <div className="App">
-      <DragDropContext onDragEnd={() => null}>
-        <DraggableComponentList listName="main" data={data} dataPath={''} />
-      </DragDropContext>
+      <DragDropComponent/>
     </div>
   );
 }
